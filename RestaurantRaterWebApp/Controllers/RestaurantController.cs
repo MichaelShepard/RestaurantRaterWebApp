e6e0116fc44a37a877cea60e82b64916b4899521
MichaelShepard <mike@rockcastlepartners.com>
2021-03-26 20:04:09 -0400

    added Edit capability

--- RestaurantRaterWebApp/Controllers/RestaurantController.cs
+++ RestaurantRaterWebApp/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaterWebApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -71,6 +72,45 @@ namespace RestaurantRaterWebApp.Controllers
 
         }
 
+        // GET: Restaurant/Edit/{id}
 
-    }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Restaurant restaurant = _db.Restaurants.Find(id);
+
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(restaurant);
+        }
+
+
+
+        // POST: Restaurant/Edit/{id}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+
+
+        }
+
+
+        }
 }
